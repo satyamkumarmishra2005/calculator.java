3651fcd3957fe7d7edeea21e89cecc8a7fe0e2e7
satyam kumar mishra <satyamkumarmishra2005@gmail.com>
2023-10-22 17:30:36 +0530

    first commit

---
+++ calculator.java
@@ -0,0 +1,50 @@
+package com.apnacollege;
+import java .util.Scanner;
+public class  calculator {
+    public static void main(String[] argS) {
+        char operator;
+        double number1, number2, result;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = sc.next().charAt(0);
+
+        System.out.println("Enter first number");
+        number1 = sc.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = sc.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        //sc.close();
+    }
+
+
+
+
+}
+
+
+
